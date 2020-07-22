7403df79c63da8f7621374bf15aebbf4b024de7e
Ash McBride <ash@ashmcbride.top>
2020-07-22 20:17:44 +0100

    :sparkles: feat(hooks): add hooks to commit

--- src/util/tokens.ts
+++ src/util/tokens.ts
@@ -1,4 +1,4 @@
-import jwt from "jsonwebtoken";
+import * as jwt from "jsonwebtoken";
 import { Response, Request, NextFunction } from "express";
 import { SESSION_SECRET } from "./secrets";
 import { RequestToken, RefreshToken } from "../types/tokens";
