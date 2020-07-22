9c0011e9c683106c2d12c62a449de20f7756da68
Ash McBride <ash@ashmcbride.top>
2020-07-22 18:25:50 +0100

    feat(hooks): add commit hooks for eslint, prettier and tsc

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
-import express from "express";
-import compression from "compression"; // compresses requests
-import bodyParser from "body-parser";
-import { Sequelize } from "sequelize";
-import { POSTGRES_URI } from "./util/secrets";
+import * as express from "express";
+import * as compression from "compression"; // compresses requests
+import * as bodyParser from "body-parser";
+import * as mongoose from "mongoose";
+import { MONGODB_URI } from "./util/secrets";
 import { validateToken } from "./util/tokens";
 import logger from "./util/logger";
 // Controllers (route handlers)
@@ -12,26 +12,23 @@ import * as tokenController from "./controllers/tokens";
 const app = express();
 app.use(validateToken);
 // Connect to MongoDB
-const dbUrl = POSTGRES_URI;
+const dbUrl = MONGODB_URI;
 
-const sequelize = new Sequelize({
-  dialect: "postgres",
-  database: "loyalty",
-  username: "",
-  password: "",
-  host: "localhost",
-  port: 5432,
-  // host: dbUrl,
-  logging: (msg) => logger.info(msg),
-});
+mongoose.connect(dbUrl, { useNewUrlParser: true, useUnifiedTopology: true });
 
-try {
-  sequelize.authenticate().then(() => {
-    console.log("Connection has been established successfully.");
+mongoose.set("debug", function (coll, method, query, doc, options) {
+  const set = {
+    coll,
+    method,
+    query,
+    doc,
+    options,
+  };
+
+  logger.info({
+    dbQuery: set,
   });
-} catch (error) {
-  console.error("Unable to connect to the database:", error);
-}
+});
 
 // Express configuration
 app.set("port", process.env.PORT || 3000);
--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -1,6 +1,6 @@
 import logger from "./logger";
-import dotenv from "dotenv";
-import fs from "fs";
+import * as dotenv from "dotenv";
+import * as fs from "fs";
 
 if (fs.existsSync(".env")) {
   logger.debug("Using .env file to supply config environment variables");
@@ -10,14 +10,14 @@ export const ENVIRONMENT = process.env.NODE_ENV;
 const prod = ENVIRONMENT === "production"; // Anything else is treated as 'dev'
 
 export const SESSION_SECRET = process.env["SESSION_SECRET"];
-export const POSTGRES_URI = prod ? process.env["POSTGRES_URI"] : process.env["POSTGRES_URI_LOCAL"];
+export const MONGODB_URI = prod ? process.env["MONGODB_URI"] : process.env["MONGODB_URI_LOCAL"];
 
 if (!SESSION_SECRET) {
   logger.error("No client secret. Set SESSION_SECRET environment variable.");
   process.exit(1);
 }
 
-if (!POSTGRES_URI) {
+if (!MONGODB_URI) {
   if (prod) {
     logger.error("No postgres connection string. Set POSTGRES_URI environment variable.");
   } else {
